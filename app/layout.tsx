48473d0cbb084af104261202f59c90c7da721e6d
shaehl <z3z_a@hotmail.com>
2024-08-30 01:39:38 +0300

    twitter:card to summary, trial and error

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,19 +7,19 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "JIULH",
   themeColor: "#747b27",
-  twitter: {
-    "images": "https://jiulh-site.vercel.app/static/jiulh.png"
-  },
   openGraph: {
     title: "JIULH",
     description: "Определённо человек",
-    siteName: "jiulh-site",
-    url: "https://jiulh-site.vercel.app/"
+    url: "https://jiulh-site.vercel.app/",
+    images: "https://jiulh-site.vercel.app/static/jiulh.png"
   },
   icons: {
     icon: '/static/jiulh.png',
     shortcut: '/static/jiulh.png',
     apple: '/static/jiulh.png'
+  },
+  twitter: {
+    card: "summary"
   }
 };
 
